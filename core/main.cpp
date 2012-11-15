619a6a86e2aba7aec688b1e1dcd88833beb249e2
Ryohei Suzuki <quolc.i@gmail.com>
2012-11-15 16:15:05 +0900

    コマンドライン引数の処理を追加

--- core/main.cpp
+++ core/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <boost/format.hpp>
+#include <boost/program_options.hpp>
 
 using namespace std;
+using namespace boost::program_options;
 
 /*
  * Basic Usage
@@ -11,19 +12,56 @@ using namespace std;
  * Remote Debugging
  * $ ./core hoge.s -r 8080  ... setup the server and wait for a connection of clients.
  *
- * Special Modes
- * $ ./core hoge.s -c       ... only check the validity of the assembly
- *
  * Simulation Options
  * -S       ... diable cycle acculate mode (fast)
  * -M 1024  ... set the memory size (W).
  */
 int main(int argc, char* argv[]) {
-    cout << "CPU Simulator for CPUEX IIDX tricoro's 2nd Architecture." << endl;
+    cout << "CPU Simulator for CPUEX IIDX tricoro's 2nd Architecture.\n" << endl;
 
-    for (int i=0; i<argc; i++) {
-        cout << argv[i] << endl;
+    // Command Line Arguments Processing
+    options_description options("Options");
+ 
+    int port = 0;
+    bool remote = false;
+    bool single = false;
+    int memory = 0;
+
+    options.add_options()
+        ("remote,r", value<int>(&port), "Remote Debug Mode")
+        ("single,s", "Single Clock Mode")
+        ("memory,M", value<int>(&memory), "Memory Size [Word]")
+        ("help,h", "Help");
+
+    variables_map vm;
+
+    try {
+        store(parse_command_line(argc, argv, options), vm);
+        notify(vm);
+
+        if (argc < 2) {
+            cout << "!! Please Specify the Assembly File Name as the First Argument. !!\n" << endl;
+            return 0;
+        }
+        if (vm.count("help")) {
+            cout << options << endl;
+            return 0;
+        }
+
+        if (vm.count("remote"))
+            remote = true;
+        if (vm.count("single"))
+            single = true;
+    } catch(exception &e) {
+        cout << options << endl;
+        cout << e.what() << endl;
     }
+
+    cout << "Assembly File: " << argv[1] << endl;
+    if (remote)
+        cout << "-- Remote Debugging Mode (port " << port << ")" << endl;
+    if (single)
+        cout << "-- Single Clock Mode" << endl;
 
     return 0;
 }
